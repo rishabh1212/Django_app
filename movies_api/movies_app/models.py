0ade1fd63a6595b2ae611604231543b4e5cd6290
rishabh1212 <rishabh6417@gmail.com>
2020-10-08 19:28:54 +0900

    define user and movies object to run

--- movies_api/movies_app/models.py
+++ movies_api/movies_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    imdb_id = models.IntegerField()
+    original_title = models.CharField(max_length=200)
+    overview = models.TextField()
+
+
+class User(models.Model):
+    user_id = models.BigIntegerField()
+    user_name = models.CharField(max_length=50)
+    favourite_movies = models.ManyToManyField(Movie)
