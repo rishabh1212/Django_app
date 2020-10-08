ee0b4f76ccc400ec70a875582abe586690138081
rishabh1212 <rishabh6417@gmail.com>
2020-10-09 05:16:44 +0900

    make imdb_id primary key and use builtin USER object to ease login authentication

--- movies_api/movies_app/models.py
+++ movies_api/movies_app/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Movie(models.Model):
-    imdb_id = models.CharField(max_length=200)
+
+class movie(models.Model):
+    imdb_id = models.CharField(max_length=200, primary_key=True)
     original_title = models.CharField(max_length=200)
     overview = models.TextField()
 
-
-class User(models.Model):
-    user_name = models.CharField(max_length=50)
-    favourite_movies = models.ManyToManyField(Movie)
+class userfavorites(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    favorite_movies = models.ManyToManyField(movie)
