50742f375aa4dccb0a9086afcfb56341c2ec53be
rishabh1212 <rishabh6417@gmail.com>
2020-10-09 07:14:09 +0900

    refactor and adding populate script in run.sh

--- movies_api/run.sh
+++ movies_api/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-cd /movies_app/movies_api
+cd /movies_api
 
 # python manage.py makemigrations
 until python manage.py makemigrations movies_app
@@ -9,15 +9,9 @@ do
   	sleep 3
   	echo "Trying again"
 done
-if  python manage.py check; then
+
 python manage.py migrate
-fi
 
-cd /app
-until python populate.py 
-do 
-	sleep 3
-done
+./manage.py shell < populate.py
 
-cd /movies_app/movies_api
-python manage.py runserver
+python manage.py runserver 0.0.0.0:8000
